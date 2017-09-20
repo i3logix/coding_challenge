89fcd0fa680ec27f1df9c1c0c1866d043671e293
Ryan Douglas <ryan@luckydogdigital.com>
2017-09-20 14:11:38 -0600

    Update app.js

--- app.js
+++ app.js
@@ -2,15 +2,4 @@
 * i3logix Code Challenge
 * 
 * Please refer to the README.md for challenge questions and complete your challenge below.
-* 
-* Steps:
-*
-* 1. Write your challenge code below.
-* 2. Export a higher order function that will accept arguments for testing
 */
-
-
-// export your function for testing
-module.exports = function higherOrderFunction() {
-
-}
