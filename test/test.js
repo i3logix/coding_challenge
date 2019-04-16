86a40a56c25710589f34d516e54a1aeed6a1e61a
Dann Stockton <dannstockton@i3logix.com>
2019-04-16 16:36:21 -0600

    Remove test

--- test/test.js
+++
@@ -1,9 +0,0 @@
-const assert = require('chai').assert;
-const application = require('../app.js');
-
-describe('Application', () => {
-
-  it('returns desired result', () => {
-    assert.deepEqual(application(arguments), 'desired result')
-  })
-});
