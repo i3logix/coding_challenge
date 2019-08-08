1fb3b4e6c1ffa10fea4ab9b831d22b77bedf9fd5
kelsh <rhwelch2009@gmail.com>
2019-08-08 02:19:48 -0600

    Add getRank method to pokerhand class, update pokerhand test

--- src/PokerHand/PokerHand.js
+++ src/PokerHand/PokerHand.js
@@ -4,12 +4,128 @@
 * Please refer to the README.md for challenge questions and complete your challenge below.
 */
 export default class PokerHand {
+
   constructor(hand) {
     this.hand = hand;
   }
 
   getRank = () => {
-    // TODO: Implement poker hand ranking
-    return 'Royal Flush';
+
+    //could move this to constructor if other methods need it.
+    let cards = this.hand.split(" ");
+    let suits = cards.map( (card) => card[card.length -1] );
+    let faces = cards.map( (card) =>  card.slice( 0 , card.length -1)  );
+    
+    //more convient to have numbers.
+    faces = faces.map( card => {
+      if (card === "J"){
+        return 11;
+      }
+      if (card === "Q"){
+        return 12;
+      }
+      if (card === "K"){
+        return 13;
+      }
+      if (card === "A"){
+        return 14;
+      }
+      
+      return parseInt(card)
+    });
+
+    //this is useful across multiple hands
+    let duplicates = {}
+    faces.forEach( (el) => {
+ 
+      if(!duplicates[el]){
+        duplicates[el] = 1;
+      }
+      else{
+        duplicates[el] = duplicates[el] + 1;
+      }
+    });
+
+    let isFlush = suits.every( suit => [suits[0]].indexOf(suit) > -1 );
+    
+    //this is not very elegant.  Not sure if there is an easier way to do it
+    faces.sort( ( (a, b) => { return a > b ? 1 : -1} ) );
+    let isStraight = faces.every( (el, index, arr) => {
+
+      if(index == 4 && el == 14 && 2 == arr[0]){
+        return true;
+      }
+
+      if(!arr[index+1] && el-1 == arr[index-1]){
+        return true
+      }
+
+      if(el+1 == arr[index+1] || (el == 5 && arr[index+1] == 14)){
+        return true;
+      }
+
+      return false;
+    })
+
+    //we can just go down from highest rank to lowest rank
+    switch (true) {
+
+      case (
+        isFlush &&
+        isStraight &&
+        faces.includes(14)
+      ) :
+
+        return "Royal Flush";
+        break;
+
+      case (
+        isFlush &&
+        isStraight
+      ) :
+
+        return "Straight Flush";
+        break;
+
+      case Object.values(duplicates).includes(4):
+        
+        return "Four of a Kind";
+        break;
+
+      case Object.values(duplicates).includes(3) && Object.values(duplicates).includes(2) :
+      
+        return "Full House";
+        break;
+
+      case isFlush :
+    
+        return "Flush";
+        break;
+
+      case isStraight :
+  
+        return "Straight";
+        break;
+      
+      case Object.values(duplicates).includes(3) :
+  
+        return "Three of a Kind";
+        break;
+
+      case Object.values(duplicates).includes(2) && Object.keys(duplicates).length === 3 :
+
+        return "Two Pair";
+        break;
+
+      case Object.values(duplicates).includes(2) && Object.keys(duplicates).length === 4 :
+
+        return "One Pair";
+        break;
+
+      default:
+        
+        return "High Card"
+    }
   };
+
 }
--- test/PokerHand/pokerHand.test.js
+++ test/PokerHand/pokerHand.test.js
@@ -8,9 +8,34 @@ describe('PokerHand', () => {
     assert.deepEqual(hand.getRank(), 'Royal Flush');
   });
 
-  it('can rank a pair', () => {
-    const hand = new PokerHand('Ah As 10c 7d 6s');
-    assert.deepEqual(hand.getRank(), 'One Pair');
+  it('can rank a Straight flush', () => {
+    const hand = new PokerHand('9s Ks Qs Js 10s');
+    assert.deepEqual(hand.getRank(), 'Straight Flush');
+  });
+
+  it('can rank a Four of a Kind', () => {
+    const hand = new PokerHand('9s 10c 10h 10d 10s');
+    assert.deepEqual(hand.getRank(), 'Four of a Kind');
+  });
+
+  it('can rank a Full House', () => {
+    const hand = new PokerHand('9s 9d 10s 10d 10c');
+    assert.deepEqual(hand.getRank(), 'Full House');
+  });
+
+  it('can rank a flush', () => {
+    const hand = new PokerHand('Kh Qh 6h 2h 9h');
+    assert.deepEqual('Flush', hand.getRank());
+  });
+
+  it('can rank a Straight', () => {
+    const hand = new PokerHand('2h 3h 4h 5d Ah');
+    assert.deepEqual('Straight', hand.getRank());
+  });
+
+  it('can rank a Three of a Kind', () => {
+    const hand = new PokerHand('Ah As Ac 7d 6s');
+    assert.deepEqual('Three of a Kind', hand.getRank());
   });
 
   it('can rank two pair', () => {
@@ -18,9 +43,14 @@ describe('PokerHand', () => {
     assert.deepEqual('Two Pair', hand.getRank());
   });
 
-  it('can rank a flush', () => {
-    const hand = new PokerHand('Kh Qh 6h 2h 9h');
-    assert.deepEqual('Flush', hand.getRank());
+  it('can rank a One Pair', () => {
+    const hand = new PokerHand('7d Qh 6h 2d 2h');
+    assert.deepEqual('One Pair', hand.getRank());
+  });
+
+  it('can rank a One Pair', () => {
+    const hand = new PokerHand('Ah As 10c 7d 6s');
+    assert.deepEqual(hand.getRank(), 'One Pair');
   });
 
   // TODO: More tests go here
